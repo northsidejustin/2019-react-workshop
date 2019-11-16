a2bb3a518a4570ff55e9f141db6a1d6339a269a6
rcreagha <rcreagha@unb.ca>
2019-11-16 19:50:52 -0400

    refactor to pull modal & playerlist out of Board

--- src/Card.js
+++ src/Card.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import "./Card.css"
+import './Card.css'
 
 class Card extends Component {
   constructor(){
---
+++ src/constants/categories.js
@@ -0,0 +1,204 @@
+export default [
+  {
+    name: "CANADA",
+    questions: [
+      {
+        value: 200,
+        question:
+          "How many provinces and territories are there in Canada?",
+        answer: "Canada has ten provinces and three territories.",
+      },
+      {
+        value: 400,
+        question: "How many time zones are there in Canada?",
+        answer: "Canada has six time zones."
+      },
+      {
+        value: 600,
+        question:
+          "True/False: Canada is the third largest country in the world?",
+        answer:
+          "False. Canada is the second largest country in the world."
+      },
+      {
+        value: 800,
+        question: "What are Canada's national sports?",
+        answer: "Hockey and lacrosse are Canada’s national sports."
+      },
+      {
+        value: 1000,
+        question:
+          "How much of the world’s renewable water does Canada have?",
+        answer: "Canada has 7% of the world's renewable fresh water."
+      }
+    ]
+  },
+  {
+    name: "WORDS WITH 5 VOWELS",
+    questions: [
+      {
+        value: 200,
+        question: "This adjective means showy & intended to impress",
+        answer: "OSTENTATIOUS"
+      },
+      {
+        value: 400,
+        question:
+          "To inject someone with a small amount of a disease so that they can become immune to it",
+        answer: "TO INOCULATE"
+      },
+      {
+        value: 600,
+        question: "Dancing Queen can feel the beat from this instrument",
+        answer: "TAMBOURINE"
+      },
+      {
+        value: 800,
+        question:
+          "It sounds like you're writing a novel but it means to empower or formally sanction an act",
+        answer: "AUTHORIZE"
+      },
+      {
+        value: 1000,
+        question: "Ecstatic happiness",
+        answer: "EUPHORIA"
+      }
+    ]
+  },
+  {
+    name: "SOUNDS LIKE IT TO ME",
+    questions: [
+      {
+        value: 200,
+        question:
+          "If your high-flying idea turns out to be a dud it lands with this sound that rhymes with dud",
+        answer: "THUD"
+      },
+      {
+        value: 400,
+        question:
+          "Hearing this hyphenated sound of tiny feet means you have kids in the house or mice",
+        answer: "PITTER-PATTER"
+      },
+      {
+        value: 600,
+        question:
+          "After a few hours in the hot sun I'm ready to drink a cold beverage this way also an engine sound",
+        answer: "CHUG"
+      },
+      {
+        value: 800,
+        question: "Large ones can measure more than 5 feet across",
+        answer: "A GONG"
+      },
+      {
+        value: 1000,
+        question: "Hey! It's a 6-letter bray from Mr. Ed",
+        answer: "WHINNY"
+      }
+    ]
+  },
+  {
+    name: "SINGLE-NAMED CELEBS",
+    questions: [
+      {
+        value: 200,
+        question:
+          "She's been caught in Peter Parker's web playing MJ in Marvel's Spider-Man flicks",
+        answer: "ZENDAYA"
+      },
+      {
+        value: 400,
+        question:
+          "In 2010 back when she still spelled her name with a dollar sign she had a No. 1 hit with We R Who We R",
+        answer: "KE$HA"
+      },
+      {
+        value: 600,
+        question:
+          "Bassist for the Red Hot Chili Peppers he's acted in films like Baby Driver & The Big Lebowski",
+        answer: "FLEA"
+      },
+      {
+        value: 800,
+        question:
+          "This British model almost had a date with Elwood in The Blues Brothers but it didn't stick",
+        answer: "TWIGGY"
+      },
+      {
+        value: 1000,
+        question:
+          "This Mogadishu-born supermodel was smokin' on the big screen opposite James Kirk in Star Trek VI",
+        answer: "IMAN"
+      }
+    ]
+  },
+  {
+    name: "MOVIE CHALLENGE",
+    questions: [
+      {
+        value: 200,
+        question:
+          "Hiccup is the hero of this animated trilogy about Vikings & the creatures they once feared",
+        answer: "HOW TO TRAIN YOUR DRAGON"
+      },
+      {
+        value: 400,
+        question: "Bo Derek played this man's stepmother in Tommy Boy",
+        answer: "CHRIS FARLEY"
+      },
+      {
+        value: 600,
+        question:
+          "In 2019 Christina Hendricks revealed that's her hand holding the rose on the poster for this 1999 Best Picture Oscar winner",
+        answer: "AMERICAN BEAUTY"
+      },
+      {
+        value: 800,
+        question:
+          "Spanish slang for hitman provides the title of this drug cartel thriller with Benicio del Toro & its Day of the Soldado sequel",
+        answer: "SICARIO"
+      },
+      {
+        value: 1000,
+        question:
+          "Name of the leopard Katharine Hepburn is Bringing Up in a screwball classic",
+        answer: "BABY"
+      }
+    ]
+  },
+  {
+    name: "RED",
+    questions: [
+      {
+        value: 200,
+        question:
+        "It's the Red Planet",
+        answer: "MARS"
+      },
+      {
+        value: 400,
+        question: "When blood goes through these organs it turns red",
+        answer: "LUNGS"
+      },
+      {
+        value: 600,
+        question:
+          "Pennyworth or E. Neuman",
+        answer: "ALFRED"
+      },
+      {
+        value: 800,
+        question:
+          "Prey's proverbial foe",
+        answer: "PREDATOR"
+      },
+      {
+        value: 1000,
+        question:
+          "The name of this cosmetic that can turn your cheeks red mean red",
+        answer: "ROUGE"
+      }
+    ]
+  }
+]
